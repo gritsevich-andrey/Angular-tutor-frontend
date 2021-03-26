1c1d29fc97fc540dcf66f9df3e97cb9af6c8bdf8
HRYTSEVICH <grand16121980@gmail.com>
2021-03-26 17:40:23 +0300

    Added pupil panel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+import {Constrants} from './constrants';
 
+export const environment: Constrants = {
+  production: false,
+  USER_API: '/users',
+  AUTH_API: '/login',
+  REG_API: '/register',
+  LESSON_ORDER_API: '/lessons/create',
+  LESSONS_API: '/lessons',
+  CATEGORY_API: '/categories',
+  INSTR_API: '/instructors',
+  PROGRAM_API: '/programs'
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
